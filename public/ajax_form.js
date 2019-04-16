690fce491ded2bc2b64934cef730d49be289fc14
N-Ryo <ryo.joy.joy@gmail.com>
2019-04-16 13:31:03 +0900

    Add color

--- public/ajax_form.js
+++ public/ajax_form.js
@@ -47,4 +47,99 @@ $(document).ready(function () {
         $appear_form.toggleClass('active');
         $form.toggleClass('form-active');
     });
+
+    var $about = $('.about');
+    var $skills = $('.skills');
+    var $works = $('.works');
+    var $about_title = $('#about-title');
+    var $skills_title = $('#skills-title');
+    var $works_title = $('#works-title');
+    var $about_content = $('#about-content');
+    var $skills_content = $('#skills-content');
+    var $works_content = $('#works-content');
+    var $about_main = $('.about-main');
+    var $skills_main = $('.skills-main');
+    var $works_main = $('.works-main');
+
+    $about.on('click', function(){
+        $about_title.toggleClass('title-active');
+        $about_title.toggleClass('title-passive');
+        $about_content.toggleClass('content-active');
+        $about_content.toggleClass('content-passive');
+        $about.toggleClass('icon-active');
+        $about.toggleClass('icon-passive');
+        $about_main.toggleClass('main-active');
+        if( $skills_title.hasClass('title-active') == true ) {
+            $skills_title.toggleClass('title-active');
+            $skills_title.toggleClass('title-passive');
+            $skills_content.toggleClass('content-active');
+            $skills_content.toggleClass('content-passive');
+            $skills.toggleClass('icon-active');
+            $skills.toggleClass('icon-passive');
+            $skills_main.toggleClass('main-active');
+        }
+        if( $works_title.hasClass('title-active') == true ) {
+            $works_title.toggleClass('title-active');
+            $works_title.toggleClass('title-passive');
+            $works_content.toggleClass('content-active');
+            $works_content.toggleClass('content-passive');
+            $works.toggleClass('icon-active');
+            $works.toggleClass('icon-passive');
+            $works_main.toggleClass('main-active');
+        }
+    });
+    $skills.on('click', function(){
+        $skills_title.toggleClass('title-active');
+        $skills_title.toggleClass('title-passive');
+        $skills_content.toggleClass('content-active');
+        $skills_content.toggleClass('content-passive');
+        $skills.toggleClass('icon-active');
+        $skills.toggleClass('icon-passive');
+        $skills_main.toggleClass('main-active');
+        if( $about_title.hasClass('title-active') == true ) {
+            $about_title.toggleClass('title-active');
+            $about_title.toggleClass('title-passive');
+            $about_content.toggleClass('content-active');
+            $about_content.toggleClass('content-passive');
+            $about.toggleClass('icon-active');
+            $about.toggleClass('icon-passive');
+            $about_main.toggleClass('main-active');
+        }
+        if( $works_title.hasClass('title-active') == true ) {
+            $works_title.toggleClass('title-active');
+            $works_title.toggleClass('title-passive');
+            $works_content.toggleClass('content-active');
+            $works_content.toggleClass('content-passive');
+            $works.toggleClass('icon-active');
+            $works.toggleClass('icon-passive');
+            $works_main.toggleClass('main-active');
+        }
+    });
+    $works.on('click', function(){
+        $works_title.toggleClass('title-active');
+        $works_title.toggleClass('title-passive');
+        $works_content.toggleClass('content-active');
+        $works_content.toggleClass('content-passive');
+        $works.toggleClass('icon-active');
+        $works.toggleClass('icon-passive');
+        $works_main.toggleClass('main-active');
+        if( $skills_title.hasClass('title-active') == true ) {
+            $skills_title.toggleClass('title-active');
+            $skills_title.toggleClass('title-passive');
+            $skills_content.toggleClass('content-active');
+            $skills_content.toggleClass('content-passive');
+            $skills.toggleClass('icon-active');
+            $skills.toggleClass('icon-passive');
+            $skills_main.toggleClass('main-active');
+        }
+        if( $about_title.hasClass('title-active') == true ) {
+            $about_title.toggleClass('title-active');
+            $about_title.toggleClass('title-passive');
+            $about_content.toggleClass('content-active');
+            $about_content.toggleClass('content-passive');
+            $about.toggleClass('icon-active');
+            $about.toggleClass('icon-passive');
+            $about_main.toggleClass('main-active');
+        }
+    });
 });
--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import topImage from './images/top-image.jpg'
 import myPicture from './images/my-pic.jpg'
+import sqlite from './images/sqlite.png'
+import jquery from './images/jquery.png'
+import ruby from './images/ruby.png'
+import rails from './images/rails.png'
+import git from './images/git.png'
+import mock from './images/mock.png'
+
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -17,26 +24,67 @@ class Frame extends React.Component {
     const style = {backgroundImage:`url(${topImage})`};
     return (
       <div className="portfolio">
-        <ul className="menu">
-          <li>
-            <div className="main">
-              <h1>Portfolio</h1>
-              <div className="profile">
-                <div className="pic-wapper">
-                  <img className="my-pic" src={myPicture} alt="Ryo Nishimura"/>
-                </div>
-                <p className="description">Thank you for visiting my portfolio. My name is Ryo Nishimura, and I live in Tokyo. I am Back-End engineer. I guraduated from Tohoku Univ. I love to create WEB services, which are fully responsive, mobile-first layouts and MVC applications. If you want more information or to ask any job, please contact me from <span className="mail-link">here</span>.</p>
+        <div className="about-main main-active">
+          <h1 id="about-title" className="title title-active">Portfolio</h1>
+          <div id="about-content" className="content content-active">
+            <div className="pic-wapper">
+              <img className="my-pic" src={myPicture} alt="Ryo Nishimura"/>
+            </div>
+            <p className="description">Thank you for visiting my portfolio. My name is Ryo Nishimura, and I live in Tokyo. I am Back-End engineer. I guraduated from Tohoku Univ. I love to create WEB services, which are fully responsive, mobile-first layouts and MVC applications. If you want more information or to ask any job, please contact me from <span className="mail-link">here</span>.</p>
+          </div>
+        </div>
+        <div className="skills-main">
+          <h1 id="skills-title" className="title title-passive">Skill Set</h1>
+          <div id="skills-content" className="skills-content content content-passive">
+            <div className="skill-content">
+              <h3>Front End:</h3>
+              <div className="skill-icons">
+                <FontAwesomeIcon icon={['fab', 'html5']} className="svg-skill-icon html-icon"/>
+                <FontAwesomeIcon icon={['fab', 'sass']} className="svg-skill-icon sass-icon"/>
+                <FontAwesomeIcon icon={['fab', 'css3-alt']} className="svg-skill-icon css-icon"/>
+                <FontAwesomeIcon icon={['fab', 'js-square']} className="svg-skill-icon js-icon"/>
+                <FontAwesomeIcon icon={['fab', 'react']} className="svg-skill-icon react-icon"/>
               </div>
             </div>
-          </li>
-          <li>skills</li>
-          <li>portfolio</li>
-        </ul>
-        <ul className="icons">
-          <li><FontAwesomeIcon icon={['fas', 'home']} /></li>
-          <li><FontAwesomeIcon icon={['fas', 'laptop-code']} /></li>
-          <li><FontAwesomeIcon icon={['fas', 'trophy']} /></li>
-        </ul>
+            <div className="skill-content">
+              <h3>Back End:</h3>
+              <div className="skill-icons">
+                <img className="skill-icon sqlite-icon" src={sqlite} alt="sqlite"/>
+              </div>
+            </div>
+            <div className="skill-content">
+              <h3>Frameworks:</h3>
+              <div className="skill-icons">
+                <img className="skill-icon jquery-icon" src={jquery} alt="jquery"/>
+                <FontAwesomeIcon icon={['fab', 'bootstrap']} className="svg-skill-icon bootstrap-icon"/>
+                <img className="skill-icon rails-icon" src={rails} alt="rails"/>
+              </div>
+            </div>
+            <div className="skill-content">
+              <h3>Programming:</h3>
+              <div className="skill-icons">
+                <img className="skill-icon ruby-icon" src={ruby} alt="ruby"/>
+              </div>
+            </div>
+            <div className="skill-content">
+              <h3>Tools:</h3>
+              <div className="skill-icons">
+                <FontAwesomeIcon icon={['fab', 'slack']} className="svg-skill-icon slack-icon"/>
+                <img className="skill-icon git-icon" src={git} alt="git"/>
+                <FontAwesomeIcon icon={['fab', 'github-square']} className="svg-skill-icon github-icon"/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="works-main">
+          <h1 id="works-title" className="title title-passive">Works</h1>
+          <div id="works-content" className="content content-passive">
+            <h3 className="work-site-title">mock</h3>
+            <img className="work-site" src={mock} alt="mock"/>
+            <p>-全エンジニアの叡智をここに-</p>
+            <span>全てのエンジニアへ向けた第二のリファレンスサイトです。</span>
+          </div>
+        </div>
         <div className="bgImage" style={style}></div>
       </div>
     );
@@ -52,9 +100,9 @@ class Sns extends React.Component {
   render() {
     return (
       <div className="sns">
-        <div className="about"><FontAwesomeIcon icon={['fas', 'home']} /></div>
-        <div className="skills"><FontAwesomeIcon icon={['fas', 'laptop-code']} /></div>
-        <div className="portfolio"><FontAwesomeIcon icon={['fas', 'trophy']} /></div>
+        <div className="about icon-active"><FontAwesomeIcon icon={['fas', 'home']} /></div>
+        <div className="skills icon-passive"><FontAwesomeIcon icon={['fas', 'laptop-code']} /></div>
+        <div className="works icon-passive"><FontAwesomeIcon icon={['fas', 'trophy']} /></div>
         <div className="twitter">
           <a href="https://twitter.com/_Ryo_Nishimura" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={['fab', 'twitter']} />
